1e8081ab77e5ca045739470b0eb1690f5e35bc5a
Gover912G <ngigipaul912@gmail.com>
2023-11-22 00:52:47 +0300

    index page dynamic

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from hotel.models import HeroSection,About,Testimony,MasterRoom
+
 # Register your models here.
+admin.site.register(HeroSection)
+admin.site.register(About)
+admin.site.register(Testimony)
+admin.site.register(MasterRoom)
--- hotel/views.py
+++ hotel/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from hotel.models import HeroSection, About,Testimony,MasterRoom
+
+
 # Create your views here.
 
+def master(request):
+    # heroslider = HeroSection.objects.all()
+    data = HeroSection.objects.all()
+    return render(request,'master.html', {"slides": data})
 def index(request):
-    return render(request,"index.html", {'nav': 'index'})
+    about = About.objects.all()[0]
+    testimony = Testimony.objects.all()
+    Mroom = MasterRoom.objects.all()
+    return render(request,"index.html", {'nav': 'index', "abouts": about, "test":testimony, "room":Mroom})
 
 def about(request):
-    return render(request, 'about.html', {"nav": "about"})
+    about = About.objects.all()[0]
+    testimony = Testimony.objects.all()
+    return render(request, 'about.html', {"nav": "about", "abouts": about, "test": testimony})
 
 def blog(request):
     return render(request, 'blog.html', {'nav': 'blog'})
@@ -15,7 +27,8 @@ def contact(request):
     return render(request, 'contact.html', {'nav': 'contact'})
 
 def rooms(request):
-    return render(request, 'rooms.html', {'nav': 'rooms'})
+    Mroom = MasterRoom.objects.all()
+    return render(request, 'rooms.html', {'nav': 'rooms', "room":Mroom})
 
 def restaurant(request):
         return render(request, 'restaurant.html', {'nav': 'restaurant'})
